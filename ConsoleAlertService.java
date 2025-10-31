80d9be4f2f16c261311a94aad844a679a2c026d8
ShivaniYadav24 <shivi2472003@gmail.com>
2025-10-31 14:29:36 +0530

    Update ConsoleAlertService.java

--- ConsoleAlertService.java
+++ ConsoleAlertService.java
@@ -3,10 +3,9 @@ package observer;
 import model.Product;
 
 public class ConsoleAlertService implements StockObserver {
-
     @Override
     public void onLowStock(Product product) {
-        System.out.println("⚠️ ALERT: Low stock for " + product.getName() +
-                " — Only " + product.getQuantity() + " left! (Threshold: " + product.getReorderThreshold() + ")");
+        System.out.println(" ALERT: Low stock for " + product.getName() +
+                " (Only " + product.getQuantity() + " left!)");
     }
 }
