34061f0bc66e1fb3c682c17858942eaed4e3a7c9
ShivaniYadav24 <shivi2472003@gmail.com>
2025-10-31 15:09:44 +0530

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Product {
+    private String id;
+    private String name;
+    private String category;
+    private int quantity;
+
+    public Product(String id, String name, String category, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.quantity = quantity;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{id='" + id + "', name='" + name + "', category='" + category + "', qty=" + quantity + "}";
+    }
+}
